8040a145faee0762b8572ab801f9186e63f064b3
Rodrigo Moreira <rodrigo.apvm@gmail.com>
2024-04-04 16:18:02 -0300

    reorg de arquivos

--- src/main/java/rocketseat/com/passin/dto/attendee/AttendeeDetails.java
+++
@@ -1,8 +0,0 @@
-package rocketseat.com.passin.dto.attendee;
-
-import java.time.LocalDateTime;
-
-public record AttendeeDetails(String id, String name, String email, LocalDateTime createdAt, LocalDateTime checkedInAt) {
-
-
-}
--- src/main/java/rocketseat/com/passin/dto/event/EventRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package rocketseat.com.passin.dto.event;
-
-public record EventRequestDTO(String title, String details, Integer maximumAttendees) {
-}
